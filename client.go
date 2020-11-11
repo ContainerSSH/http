61004579148499fbcf72b421f2ddde1afd7d2475
Janos Pasztor <janos@pasztor.at>
2020-11-11 23:02:34 +0100

    Comment updates for next release.

--- client.go
+++ client.go
@@ -15,12 +15,12 @@ type Client interface {
 // ClientConfiguration is the configuration structure for HTTP clients
 type ClientConfiguration struct {
 	// URL is the base URL for requests.
-	Url string `json:"url" yaml:"url" comment:"URL of the authentication server."`
+	Url string `json:"url" yaml:"url" comment:"Base URL of the server to connect."`
 	// CaCerts is either the CA certificate to expect on the server in PEM format
 	//         or the name of a file containing the PEM.
 	CaCert string `json:"cacert" yaml:"cacert" comment:"CA certificate in PEM format to use for host verification. Note: due to a bug in Go on Windows this has to be explicitly provided."`
 	// Timeout is the time the client should wait for a response.
-	Timeout time.Duration `json:"timeout" yaml:"timeout" comment:"Timeout in seconds" default:"2s"`
+	Timeout time.Duration `json:"timeout" yaml:"timeout" comment:"HTTP call timeout." default:"2s"`
 	// ClientCert is a PEM containing an x509 certificate to present to the server or a file name containing the PEM.
 	ClientCert string `json:"cert" yaml:"cert" comment:"Client certificate file in PEM format."`
 	// ClientKey is a PEM containing a private key to use to connect the server or a file name containing the PEM.
